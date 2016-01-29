ab25fbe17b205902941340502bc3af3b22b0d8a8
yidasanqian <yidasanqian@gmail.com>
2016-01-29 19:55:01 +0800

    实现递归前序遍历二叉树

--- src/me/dengfengdecao/tree/BinaryTree.java
+++ src/me/dengfengdecao/tree/BinaryTree.java
@@ -1,5 +1,7 @@
 package me.dengfengdecao.tree;
 
+import org.junit.Test;
+
 public class BinaryTree {
 	
 	/**
@@ -21,8 +23,9 @@ public class BinaryTree {
 		
 	}
 	
-	private TreeNode root;
-	
+	private TreeNode root;	// 根节点
+
+
 	/**
 	 * 递归构建二叉树
 	 * @param node
@@ -33,7 +36,7 @@ public class BinaryTree {
 			root = new TreeNode(value);
 		} else {
 			if (node.value > value) {	// 如果值小于根节点的值则放在左子树，否则放在右子树
-				if (node.left == null) {	// 如果左子树为空则实例化一个
+				if (node.left == null) {	// 如果左子树为空则实例化一个,否则递归构建左子树
 					node.left = new TreeNode(value);					
 				} else {
 					buildBinaryTree(node.left, value);						
@@ -49,6 +52,24 @@ public class BinaryTree {
 		}
 	}
 
+	// 递归前序遍历
+	void preorder(TreeNode node) {
+		if (node != null) {
+			System.out.print(node.value + "\t");
+			preorder(node.left);
+			preorder(node.right);
+		}
+	}
 	
+	// 用例1
+	@Test
+	public void test1() {
+		int t[] = {2, 4, 12, 45, 21, 6, 111, 1}; 
+		for (int i=0; i<t.length; i++) {
+			buildBinaryTree(root, t[i]);
+		}
+		preorder(root);
+		
+	}
 	
 }
